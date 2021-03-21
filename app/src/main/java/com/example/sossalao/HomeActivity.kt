630f48b4a621284245b84ad76af9c04377dab655
brNakano (bruno Nakano) <bruno.nakanoo@gmail.com>
2021-03-20 23:23:36 -0300

    progressbar working

--- app/src/main/java/com/example/sossalao/HomeActivity.kt
+++ app/src/main/java/com/example/sossalao/HomeActivity.kt
@@ -3,8 +3,14 @@ package com.example.sossalao
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.SystemClock
+import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_home.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 
 class HomeActivity : AppCompatActivity() {
@@ -22,10 +28,19 @@ class HomeActivity : AppCompatActivity() {
             when (menuItem.itemId) {
                 R.id.action_search -> Toast.makeText(this@HomeActivity,"Deseja pesquisar algo?", Toast.LENGTH_SHORT).show()
                 R.id.action_config -> startActivity(Intent(this, SettingsActivity::class.java))
-                R.id.action_refresh -> R.animator.progressbar.apply{ 10 }
+                R.id.action_refresh -> loading()
                 R.id.action_logout -> startActivity(Intent(this, LoginActivity::class.java))
             };   true
 
+        }
+
+    }
+
+    private fun loading(){
+        GlobalScope.launch(context = Dispatchers.Main) {
+            progressBar.visibility = View.VISIBLE
+            delay(10000)
+            progressBar.visibility = View.GONE
         }
     }
 }
